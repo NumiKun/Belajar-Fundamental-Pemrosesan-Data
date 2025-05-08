199789a940259f446c76bcd4392472523666b84e
NumiKun <nugrohorizki20@gmail.com>
2025-05-08 10:30:56 +0700

    add files

---
+++ extracting-txt/main.py
@@ -0,0 +1,7 @@
+my_file = open("visi_dicoding.txt", "r")
+print(my_file.read())
+my_file.close()
+ 
+# Output:
+# Visi Dicoding adalah menjadi platform edukasi teknologi terdepan yang mendorong akses literasi digital yang lebih luas untuk semua. 
+# Dicoding memiliki misi untuk mengakselerasi transisi Indonesia menuju dunia digital melalui pendidikan teknologi yang mentransformasi kehidupan.
